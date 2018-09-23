9b3bbbe73f9b0a9cbf0ad8ab0346592421632041
kvdoherty <kvdoherty@gmail.com>
2018-09-23 18:21:58 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-attr_accessor :sender, :receiver, :status, :amount
+attr_accessor :sender, :receiver, :status, :amount, :sendervalidity, :receivervalidity
 
 def initialize(sender, receiver, amount)
   @sender = sender
@@ -13,8 +13,8 @@ end
 def valid?
   sender = BankAccount.new(@sender)
   receiver = BankAccount.new(@receiver)
-  sendervalidity = sender.valid?
-  receivervalidity = receiver.valid?
+  @sendervalidity = sender.valid?
+  @receivervalidity = receiver.valid?
     if sendervalidity == true && receivervalidity == true
       true 
     else 
