07a814f5dcf5ec23db37fe32a57ff11bc24960f1
kvdoherty <kvdoherty@gmail.com>
2018-09-23 18:49:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,7 @@ def execute_transaction
 end 
 
 def reverse_transfer
+  if @status == "complete"
   @status = "reversed"
   @sender.balance = @sender.balance + @amount
   @receiver.balance = @receiver.balance - @amount
