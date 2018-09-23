08b0c416b535116704176eccb9b11d5d5ff3e85f
kvdoherty <kvdoherty@gmail.com>
2018-09-23 18:29:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,8 @@ def execute_transaction
     @sender.balance = @sender.balance - @amount 
     @receiver.balance = @receiver.balance + @amount
     @status = "complete"
+  else 
+    "Transaction rejected. Please check your account balance."
   end
 end 
 
