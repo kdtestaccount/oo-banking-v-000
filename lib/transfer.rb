11e18e034930713ad556d54332bb299dfbe81fc5
kvdoherty <kvdoherty@gmail.com>
2018-09-23 18:32:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ def execute_transaction
     @status = "complete"
   else 
     @status = "rejected"
-    "Transaction rejected. Please check your account balance."
+    puts "Transaction rejected. Please check your account balance."
   end
 end 
 
