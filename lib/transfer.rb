5ba1f13d8cf0e0582a146a602a1886b02fb29380
kvdoherty <kvdoherty@gmail.com>
2018-09-23 18:42:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def valid?
 end 
 
 def execute_transaction
-  if  @sender.valid? == true
+  if @status == "pending" && @sender.valid? == true
     @sender.balance = @sender.balance - @amount 
     @receiver.balance = @receiver.balance + @amount
     @status = "complete"
